3d24f1a40f7c8b93d4cdddf0e73aa5064b307356
Sean Sparkman <sean.sparkman@outlook.com>
2014-08-27 10:46:31 -0500

    added include linker class to make ios events work

---
+++ nuget/iOS/IncludeLinker.cs.pp
@@ -0,0 +1,13 @@
+using System;
+using Cookies.iOS.Views;
+
+namespace $rootnamespace$
+{
+    public class IncludeLinker
+    {
+        public void Include(CookieWebViewRenderer renderer)
+        {
+            renderer.Add(null);
+        }
+    }
+}
