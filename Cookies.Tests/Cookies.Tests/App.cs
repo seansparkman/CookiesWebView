43ab98e6d4937b9116c7e26b15c29763e1c46bc3
Sean Sparkman <sean.sparkman@outlook.com>
2014-12-19 14:08:59 -0600

    updated the testing application

--- Cookies.Tests/Cookies.Tests/App.cs
+++ Cookies.Tests/Cookies.Tests/App.cs
@@ -10,30 +10,44 @@ namespace Cookies.Tests
 {
     public class App
     {
+        private static Label _label;
         public static Page MainPage
         {
             get
             {
+                _label = new Label
+                {
+                    HorizontalOptions = LayoutOptions.Center,
+                    Text = "Loading..."
+                };
+
                 var webView = new CookieWebView
                 {
                     Source = new UrlWebViewSource
                     {
                         Url = "http://dfwmobile.net"
-                    }
+                    },
+                    VerticalOptions = LayoutOptions.FillAndExpand
                 };
 
                 webView.Navigated += (sender, args) =>
                 {
                     Debug.WriteLine("Finished navigation to: {0}, Cookies: {1}", args.Url, args.Cookies.Count);
+                    _label.Text = args.Url;
                 };
                 webView.Navigating += (sender, args) =>
                 {
                     Debug.WriteLine("Navigating to: {0}", args.Url);
+                    _label.Text = args.Url;
                 };
 
                 return new ContentPage
                 {
-                    Content = webView
+                    Content = new StackLayout
+                    {
+                        Orientation = StackOrientation.Vertical,
+                        Children = { _label, webView }
+                    }
                 };
             }
         }
